52bcdcc4fb9e249cb8715e215702aeb1067c6114
Ashish Acharya <ashish.acharya14@gmail.com>
2017-02-09 09:52:23 +0545

    changed db names temporarily for testing purposes

--- nepal_geonode/local_settings.py
+++ nepal_geonode/local_settings.py
@@ -15,18 +15,18 @@ SITEURL = "http://localhost:8000/"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nepalnode',
-        'USER': 'nepalnode',
-        'PASSWORD': 'nepalnode',
+        'NAME': 'nepal_geonode',
+        'USER': 'nepal_geonode',
+        'PASSWORD': 'nepal_geonode',
     },
 
     # vector datastore for uploads
     'datastore': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         # 'ENGINE': '',  # Empty ENGINE name disables
-        'NAME': 'nepalnode-imports',
-        'USER': 'nepalnode',
-        'PASSWORD': 'nepalnode',
+        'NAME': 'nepal_geonode_app',
+        'USER': 'nepal_geonode',
+        'PASSWORD': 'nepal_geonode',
         'HOST': 'localhost',
         'PORT': '5432',
     }
