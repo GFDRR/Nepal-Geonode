c1e694745a6ac218bd8d623a4534ffb2ef205633
Sujit Maharjan <sujitmhj@gmail.com>
2021-08-22 18:35:17 +0545

    fixes in letsencrypt

--- scripts/spcgeonode/nginx/docker-entrypoint.sh
+++ scripts/spcgeonode/nginx/docker-entrypoint.sh
@@ -9,13 +9,13 @@ echo "STARTING NGINX ENTRYPOINT ---------------------------"
 date
 
 # We make the config dir
-mkdir -p "/spcgeonode-certificates/$LETSENCRYPT_MODE"
+mkdir -p "/geonode-certificates/$LETSENCRYPT_MODE"
 
 echo "Creating autoissued certificates for HTTP host"
-if [ ! -f "/spcgeonode-certificates/autoissued/privkey.pem" ] || [[ $(find /spcgeonode-certificates/autoissued/privkey.pem -mtime +365 -print) ]]; then
+if [ ! -f "/geonode-certificates/autoissued/privkey.pem" ] || [[ $(find /geonode-certificates/autoissued/privkey.pem -mtime +365 -print) ]]; then
         echo "Autoissued certificate does not exist or is too old, we generate one"
-        mkdir -p "/spcgeonode-certificates/autoissued/"
-        openssl req -x509 -nodes -days 1825 -newkey rsa:2048 -keyout "/spcgeonode-certificates/autoissued/privkey.pem" -out "/spcgeonode-certificates/autoissued/fullchain.pem" -subj "/CN=${HTTP_HOST:-null}" 
+        mkdir -p "/geonode-certificates/autoissued/"
+        openssl req -x509 -nodes -days 1825 -newkey rsa:2048 -keyout "/geonode-certificates/autoissued/privkey.pem" -out "/geonode-certificates/autoissued/fullchain.pem" -subj "/CN=${HTTP_HOST:-null}" 
 else
         echo "Autoissued certificate already exists"
 fi
@@ -24,12 +24,12 @@ echo "Creating symbolic link for HTTPS certificate"
 # for some reason, the ln -f flag doesn't work below...
 # TODO : not DRY (reuse same scripts as docker-autoreload.sh)
 rm -f /certificate_symlink
-if [ -f "/spcgeonode-certificates/$LETSENCRYPT_MODE/live/$HTTPS_HOST/fullchain.pem" ] && [ -f "/spcgeonode-certificates/$LETSENCRYPT_MODE/live/$HTTPS_HOST/privkey.pem" ]; then
+if [ -f "/geonode-certificates/$LETSENCRYPT_MODE/live/$HTTPS_HOST/fullchain.pem" ] && [ -f "/geonode-certificates/$LETSENCRYPT_MODE/live/$HTTPS_HOST/privkey.pem" ]; then
         echo "Certbot certificate exists, we symlink to the live cert"
-        ln -sf "/spcgeonode-certificates/$LETSENCRYPT_MODE/live/$HTTPS_HOST" /certificate_symlink
+        ln -sf "/geonode-certificates/$LETSENCRYPT_MODE/live/$HTTPS_HOST" /certificate_symlink
 else
         echo "Certbot certificate does not exist, we symlink to autoissued"
-        ln -sf "/spcgeonode-certificates/autoissued" /certificate_symlink
+        ln -sf "/geonode-certificates/autoissued" /certificate_symlink
 fi
 
 echo "Sanity checks on http/s ports configuration"
